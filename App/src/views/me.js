8880b752120e5b3dee6ea975e8fae455d74eb0c8
hez <626815494@qq.com>
2023-02-19 22:46:20 +0800

    用户信息视图

--- App/src/views/me.js
+++ App/src/views/me.js
@@ -1,7 +1,26 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
+import {_User, _UserDelete, _UserPOST, _UserPUT} from "../models/_user";
 
 export function Me({}){
     return <View>
-        <Text> 我的 </Text>
+        <View>
+            <Text> Api 请求范式 </Text>
+            <Button title={'POST登陆'} onPress={async () => {
+                let data = await _UserPOST()
+                alert(data.code + data.msg)
+            }}/>
+            <Button title={'Get请求个人信息'} onPress={async () => {
+                let data  = await _User()
+                alert(data.code + data.msg)
+            }}/>
+            <Button title={'put修改用户信息'} onPress={async () => {
+                let data = await _UserPUT()
+                alert(data)
+            }}/>
+            <Button title={'Delete注销用户'} onPress={async () => {
+                let data = await _UserDelete()
+                alert(data)
+            }}/>
+        </View>
     </View>
 }
