4776a796b3e79ab100a8b4ea8bd412545165ac03
hez <626815494@qq.com>
2023-02-19 22:41:07 +0800

    用户数据模型

--- Api/models/users.js
+++ Api/models/users.js
@@ -1,1 +1,13 @@
 // 用户信息模块
+const mongoose = require('mongoose')
+const schema = mongoose.Schema;
+
+const newSchema = new schema({
+    id:{type:Number},
+    tel:{type:Number,required: true},
+    name:{type:String}
+}, {timestamps: true})
+
+const User = mongoose.model("User", newSchema, "user")
+
+module.exports = User
