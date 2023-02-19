bf81d0ebef52e37ad3a89180bd1c318b123d2994
hez <626815494@qq.com>
2023-02-19 22:41:58 +0800

    用户数据模型操作

--- Api/routes/users.js
+++ Api/routes/users.js
@@ -3,11 +3,28 @@ const router = require('koa-router')()
 router.prefix('/users')
 
 router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+  ctx.body = {
+    code:200,
+    msg:'用户信息，【请求成功】'
+  }
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.post('/', function (ctx, next) {
+  ctx.body = {
+    code:200,
+    token:'A123232d2232323',
+    msg:'登陆成功'
+  }
 })
+
+router.put('/', function (ctx, next) {
+  ctx.status = 201
+})
+
+router.delete('/', function (ctx, next) {
+  ctx.status = 204
+})
+
+
 
 module.exports = router
